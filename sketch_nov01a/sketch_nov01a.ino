df536b649b7b4100885ebbe42cd9b97fcd37d65e
Emika Ijuin <emikaijuin@gmail.com>
2018-12-14 11:39:13 +0800

    first commit; arduino joins the arduino channel of an RoR app

---
+++ sketch_nov01a/sketch_nov01a.ino
@@ -0,0 +1,92 @@
+#include <ESP8266WiFi.h>
+#include <WebSocketsClient.h>
+#include <ArduinoJson.h>
+#include <EEPROM.h>
+
+
+// Initialize pins
+int redpin = D0;
+int greenpin = D2;
+int bluepin = D4;
+
+//// Connecting to the internet
+const char* ssid = "*****";
+const char* password = "*****";
+
+// Setting up the websocket client
+WebSocketsClient webSocket;
+
+// Set up the WiFi client;
+WiFiClient client;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  
+  pinMode(redpin, OUTPUT);
+  pinMode(bluepin, OUTPUT);
+  pinMode(greenpin, OUTPUT);
+  
+
+  delay(10);
+  WiFi.begin(ssid, password);
+  while(WiFi.status()!= WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);  
+  }
+  Serial.println("");
+  Serial.print("IP Address: ");
+  Serial.print(WiFi.localIP() + "\n");
+  Serial.print(WiFi.macAddress() + "\n");
+
+  // Initializing the WS2812B communication
+  setRgb(255,80,90);
+
+  // Initializing the websocket connection
+
+  // When in development, replacement first argument in webSocket.begin() function with your machine's IP address for your Wi-Fi network. Make sure to run your open your local server up to other devices by running `rails s -b 0.0.0.0` so your arduino can connect in.
+
+  // When in development, replace the below with your production URL and port. E.g. webSocket.begin("nodemcu-led-lights.herokuapp.com", 80, "/cable")
+  webSocket.begin("*****",3000, "/cable" );
+  webSocket.onEvent(webSocketEvent);
+  webSocket.setReconnectInterval(5);
+
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  webSocket.loop();
+
+} 
+
+void webSocketEvent(WStype_t type, uint8_t *payload, size_t length) {
+  switch(type) {
+    Serial.write(type);
+    case WStype_DISCONNECTED:
+      Serial.printf("[WSc] Disconnected!\n");
+      break;
+    case WStype_CONNECTED:
+      webSocket.setExtraHeaders("Content-Type: application/json");
+      // The Arduino is currently configured to join one channel. In future, if you would like to expand to include many times of smart-devices, you could expand your channels and dynamically include the channel name in the below string depending on the nature of your device. 
+      webSocket.sendTXT("{\"command\":\"subscribe\",\"identifier\":\"{\\\"channel\\\":\\\"ArduinoChannel\\\"}\"}");
+      Serial.printf("[WSc] Connected to url: %s\n", payload);
+      break;
+    case WStype_TEXT:
+      Serial.printf("[WSc] Received text: %s\n", payload);
+      DynamicJsonBuffer jBuffer;
+      JsonObject &root = jBuffer.parseObject(payload);
+      if (root["message"]["rgb"]){
+        setRgb(root["message"]["rgb"]["r"],root["message"]["rgb"]["g"],root["message"]["rgb"]["b"]);
+      }
+      break;
+  }
+}
+
+  void setRgb(uint8_t r, uint8_t g, uint8_t b) {
+   Serial.print(r);
+   Serial.print(g);
+   Serial.print(b);
+  analogWrite(redpin, r);
+  analogWrite(bluepin, b);
+  analogWrite(greenpin, g);
+  delay(10);
+}
